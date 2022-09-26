bede695321bb0784c1515a69232a2002c43cf996
plaristote <michael@unetresgrossebite.com>
2022-09-27 00:57:13 +0200

    fix linking issues

--- libcrails-sidekix/crails/sidekix.cpp
+++ libcrails-sidekix/crails/sidekix.cpp
@@ -22,4 +22,17 @@ namespace Sidekix
       name, params
     );
   }
+
+  string async_task(const string& name, DataTree& params) { return async_task(name, params.as_data()); }
+  string async_task(const string& name) { return async_task(name, DataTree()); }
+  string schedule_task(time_t timestamp, const string& name, DataTree& params) { return schedule_task(timestamp, name, params.as_data()); }
+  string schedule_task(time_t timestamp, const string& name) { return schedule_task(timestamp, name, DataTree()); }
+  string schedule_task(chrono::time_point<chrono::system_clock> timepoint, const string& name, Data params) { return schedule_task(chrono::system_clock::to_time_t(timepoint), name, params); }
+  string schedule_task(chrono::time_point<chrono::system_clock> timepoint, const string& name, DataTree& params) { return schedule_task(timepoint, name, params.as_data()); }
+  string schedule_task(chrono::time_point<chrono::system_clock> timepoint, const string& name) { return schedule_task(timepoint, name, DataTree()); }
+  string schedule_task_in(time_t interval, const string& name, DataTree& params) { return schedule_task_in(interval, name, params.as_data()); }
+  string schedule_task_in(time_t interval, const string& name) { return schedule_task_in(interval, name, DataTree()); }
+  string schedule_task_in(chrono::seconds interval, const string& name, Data params) { return schedule_task_in(interval.count(), name, params); }
+  string schedule_task_in(chrono::seconds interval, const string& name, DataTree& params) { return schedule_task_in(interval.count(), name, params); }
+  string schedule_task_in(chrono::seconds interval, const string& name) { return schedule_task_in(interval.count(), name); }
 }
